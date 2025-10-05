18dfcd7834111375a350b78b650a9b393cc46d9a
Mieruko <111077174+Mieruko@users.noreply.github.com>
2025-10-05 10:51:00 +0000

    clear

--- backend/src/routes/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getRevenue, getTopFoods } = require("../controllers/reportController");
-const router = express.Router();
-
-router.get("/revenue", getRevenue);
-router.get("/top-foods", getTopFoods);
-
-module.exports = router;
